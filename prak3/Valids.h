2a0df7ef6f22ee0809609ec8afdb1ca07f079f18
wintersxul <127469851+wintersxul@users.noreply.github.com>
2023-12-23 02:18:46 +0700

    Update Valids.h
    -

--- prak3/Valids.h
+++ prak3/Valids.h
@@ -9,7 +9,7 @@ struct Valids {
     const int month[2] = { 1, 12 };
     const int day[2] = { 1 , 31 };
     const int temperature[2] = { -60, 40 };
-    vector<string> cities = { "Moskva", "Sankt-Peterburg",
+    const vector<string> cities = { "Moskva", "Sankt-Peterburg",
     "Novosibirsk", "Ekaterinburg", "Kazan", "Nijniy Novgorod",
     "Chelyabinsk", "Samara", "Ufa", "Rostov-na-Donu", "Krasnodar",
     "Omsk", "Voronej", "Perm", "Volgograd", "Krasnoyarsk"};
